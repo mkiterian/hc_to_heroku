93fba7667d14270f06645c3b0e515bee2ad9ca1f
mkiterian <hmukite@gmail.com>
2017-07-19 08:47:32 +0300

    [Chore #12345] Fix add whitenoise configs

--- hc/wsgi.py
+++ hc/wsgi.py
@@ -10,14 +10,8 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hc.settings")
-
-
 from whitenoise.django import DjangoWhiteNoise
 
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hc.settings")
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
-
-application = get_wsgi_application()
